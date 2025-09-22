ab6dd443a22e7557b4aa613780bcaddff371248f
Enrique Prada <enrique.pdg@gmail.com>
2025-09-21 22:49:20 -0500

    Primer commit

---
+++ src/main/java/pe/edu/utp/uni/app/rest/ClienteController.java
@@ -0,0 +1,30 @@
+package pe.edu.utp.uni.app.rest;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    private final Map<Integer, String> clientes = new HashMap<>();
+    private int currentId = 1;
+
+    @PostMapping
+    public String crearCliente(@RequestBody String nombre) {
+        clientes.put(currentId++, nombre);
+        return "Cliente guardado: " + nombre;
+    }
+
+    @GetMapping
+    public Map<Integer, String> listarClientes() {
+        return clientes;
+    }
+
+    @DeleteMapping("/{id}")
+    public String eliminarCliente(@PathVariable int id) {
+        return clientes.remove(id) != null ? "Eliminado" : "No encontrado";
+    }
+}
---
+++ src/test/java/pe/edu/utp/uni/app/ProyectoUniversidadBackendNoBdApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.uni.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoUniversidadBackendNoBdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
